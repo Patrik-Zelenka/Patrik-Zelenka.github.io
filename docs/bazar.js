210d0786b8e49062d0868f09e115b5e750aa6dfa
Michal Miky Jankovský <jankovsky.michal@gmail.com>
2024-05-06 19:55:15 +0200

    render items from json

--- docs/bazar.js
+++ docs/bazar.js
@@ -9,20 +9,6 @@ window.showLoader = function() {
 }
 
 itemTemplate = function(item) {
-    //  {
-    //     "id": 45669,
-    //     "type": "RAM",
-    //     "image": "images/ram.jpg",
-    //     "name": "RAM 16GB DDR4",
-    //     "description": "16GB DDR4 RAM, 3200MHz, CL16, 1.35V, XMP 2.0",
-    //     "longDescription": "Amazing fast RAM for your PC. 16GB DDR4 RAM, 3200MHz, CL16, 1.35V, XMP 2.0 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda aut consectetur corporis culpa earum eos explicabo illo impedit itaque iusto maiores minus necessitatibus nobis odio officia pariatur possimus praesentium quod quos reiciendis repellendus sapiente sequi similique suscipit, totam veniam voluptas? Cum dignissimos doloremque doloribus libero, nam non quae reprehenderit!",
-    //     "tags": [
-    //       "RAM",
-    //       "DDR4",
-    //       "16GB"
-    //     ],
-    //     "price": "1500"
-    //   },
     return `<div class="item">
                 <div class="item-image" style="background-image: url('${item.image}')">
                 </div>
